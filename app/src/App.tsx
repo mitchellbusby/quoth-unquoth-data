b7812af8375bc2f83e8118511a39821a632fc320
Mitchell Busby <mitchbusby@gmail.com>
2022-08-20 01:12:33 +1000

    add controls

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client";
+import { Controls } from "./Controls";
 import { OpenLayersMap } from "./OpenLayersMap";
 
 const App = () => {
   return (
     <div>
       <OpenLayersMap />
+      <Controls />
     </div>
   );
 };
