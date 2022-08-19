719109364a1c337b48d76310bccd8e995c520d4a
Mitchell Busby <mitchbusby@gmail.com>
2022-08-20 01:30:00 +1000

    add animating of controls

--- app/src/Controls.tsx
+++ app/src/Controls.tsx
@@ -1,6 +1,8 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { useState } from "react";
 
-const ControlsElement = styled.div({
+const ControlsElement = styled.div(() => ({
   background: "white",
   width: "200px",
   height: "200px",
@@ -10,6 +12,43 @@ const ControlsElement = styled.div({
   borderRadius: "var(--surface-border-radius)",
   boxShadow: "var(--shadow-elevation-medium)",
   padding: "var(--space-s)",
-});
+  transition: "all 400ms ease",
+}));
 
-export const Controls = () => <ControlsElement>Controls</ControlsElement>;
+export const Controls = () => {
+  const [hidden, setHidden] = useState(false);
+  return (
+    <>
+      <ControlsElement
+        css={
+          hidden
+            ? css`
+                transform: translateX(300px);
+              `
+            : css`
+                transform: none;
+              `
+        }
+      >
+        Controls <button onClick={() => setHidden(true)}>Hide</button>
+      </ControlsElement>
+      {hidden && (
+        <div
+          css={css`
+            position: fixed;
+            bottom: 8px;
+            right: 8px;
+          `}
+        >
+          <button
+            onClick={() => {
+              setHidden(false);
+            }}
+          >
+            {"Show"}
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
