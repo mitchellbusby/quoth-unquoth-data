779a170d24c91b8f9c52673d114dd1963cd3cba8
Mitchell Busby <mitchbusby@gmail.com>
2022-08-20 00:39:39 +1000

    extract controls

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { OpenLayersMap } from "./OpenLayersMap";
-import { css } from "@emotion/react";
-import styled from "@emotion/styled";
-
-const Controls = styled.div({
-  background: "white",
-  width: "200px",
-  height: "200px",
-  position: "fixed",
-  right: "var(--space-s)",
-  bottom: "var(--space-s)",
-  borderRadius: "var(--surface-border-radius)",
-  boxShadow: "var(--shadow-elevation-medium)",
-  padding: "var(--space-s)",
-});
 
 const App = () => {
   return (
     <div>
       <OpenLayersMap />
-      <Controls>Controls</Controls>
     </div>
   );
 };
