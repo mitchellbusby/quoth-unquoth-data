b03a9950ec548b711f36f94a0dd402382446e7ea
Mitchell Busby <mitchbusby@gmail.com>
2022-08-20 01:40:02 +1000

    add blue bus

--- app/src/buses.ts
+++ app/src/buses.ts
@@ -1,10 +1,14 @@
-export type BusType = "bus" | "bus_old" | "bus_pride";
+export type BusType = "bus" | "bus_old" | "bus_pride" | "bus_blue";
 
-export const BusList = [
+export const BusList: { label: string; id: BusType }[] = [
   { label: "Standard bus", id: "bus" },
   { label: "Old bus", id: "bus_old" },
   {
     label: "Pride bus",
     id: "bus_pride",
+  },
+  {
+    label: "Blue bus",
+    id: "bus_blue",
   },
 ];
