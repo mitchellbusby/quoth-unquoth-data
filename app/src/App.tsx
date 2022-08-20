3eebd3026be0693605833feae45934530777548e
Mitchell Busby <mitchbusby@gmail.com>
2022-08-20 10:43:26 +1000

    use bus type

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
+import { BusType } from "./buses";
 import { Controls } from "./Controls";
 import { OpenLayersMap } from "./OpenLayersMap";
 
 const App = () => {
-  const [selectedBus, setSelectedBus] = useState<
-    "bus" | "bus_old" | "bus_pride"
-  >("bus");
+  const [selectedBus, setSelectedBus] = useState<BusType>("bus");
 
   return (
     <div>
