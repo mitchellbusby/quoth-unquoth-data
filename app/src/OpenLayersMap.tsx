36da121cd9aa25811cc74e250590ff9d250e280a
Joey Tuong <joey@tetrap.us>
2022-08-20 16:30:16 +1000

    Fix dodgy refactor

--- app/src/OpenLayersMap.tsx
+++ app/src/OpenLayersMap.tsx
@@ -11,8 +11,45 @@ import { Style, Icon } from "ol/style";
 import busRoutes from "./data/stop_times.json";
 import stops from "./data/stops.json";
 import VectorLayer from "ol/layer/Vector";
+import Bus from "./static/bus.png";
+import RightBus from "./static/right-bus.png";
+import OldBus from "./static/bus_old.png";
+import RightOldBus from "./static/right-bus_old.png";
+import LongBus from "./static/bus_long.png";
+import RightLongBus from "./static/right-bus_long.png";
+import BlueBus from "./static/bus_blue.png";
+import RightBlueBus from "./static/right-bus_blue.png";
+import PrideBus from "./static/bus_pride.png";
+import RightPrideBus from "./static/right-bus_pride.png";
+
+const busTypes = {
+  new: {
+    prob: 63,
+    left: new Style({ image: new Icon({ src: Bus }) }),
+    right: new Style({ image: new Icon({ src: RightBus }) }),
+  },
+  old: {
+    prob: 23,
+    left: new Style({ image: new Icon({ src: OldBus }) }),
+    right: new Style({ image: new Icon({ src: RightOldBus }) }),
+  },
+  long: {
+    prob: 8,
+    left: new Style({ image: new Icon({ src: LongBus }) }),
+    right: new Style({ image: new Icon({ src: RightLongBus }) }),
+  },
+  blue: {
+    prob: 5,
+    left: new Style({ image: new Icon({ src: BlueBus }) }),
+    right: new Style({ image: new Icon({ src: RightBlueBus }) }),
+  },
+  pride: {
+    prob: 1,
+    left: new Style({ image: new Icon({ src: PrideBus }) }),
+    right: new Style({ image: new Icon({ src: RightPrideBus }) }),
+  },
+};
 import { AppStateContext } from "./AppState";
-import { getBusStyle } from "./getBusStyle";
 import BusStop from "./static/stop.png";
 
 const days = [
@@ -29,6 +66,26 @@ const secondsInADay = 24 * 60 * 60;
 const secondsInAWeek = 7 * secondsInADay;
 
 const busStopStyle = new Style({ image: new Icon({ src: BusStop }) });
+function hashCode(value: string) {
+  var hash = 0,
+    i = 0,
+    len = value.length;
+  while (i < len) {
+    hash = ((hash << 5) - hash + value.charCodeAt(i++)) << 0;
+  }
+  return hash;
+}
+
+const busImageStyle = new Style({
+  // image: new CircleStyle({
+  //   radius: 5,
+  //   fill: new Fill({ color: "yellow" }),
+  //   stroke: new Stroke({ color: "red", width: 1 }),
+  // }),
+  image: new Icon({
+    src: Bus,
+  }),
+});
 
 function interpolate(a: Coordinate, b: Coordinate, frac: number) {
   var nx = a[0] + (b[0] - a[0]) * frac;
@@ -43,6 +100,10 @@ function getStopLocation(stopId: number) {
 const OpenLayersMap = () => {
   const appState = useContext(AppStateContext);
   const mapRef = createRef<HTMLDivElement>();
+  const [timeOfDay, setTimeOfDay] = useState<number>(0);
+  useEffect(() => {
+    appState.frameCount = timeOfDay;
+  }, [timeOfDay]);
 
   useEffect(() => {
     if (!mapRef.current) {
@@ -57,7 +118,25 @@ const OpenLayersMap = () => {
       style: (feature) => {
         const properties = feature.getProperties();
         if (properties.type === "bus") {
-          let style = getBusStyle(properties);
+          const roll = Math.abs(hashCode(properties.tripId) % 100);
+          let cumsum = 0;
+          const options = Object.keys(busTypes);
+          let option = options.pop();
+          while (roll > cumsum) {
+            option = options.pop();
+            cumsum += busTypes[option].prob;
+          }
+          let rotation = (2 * Math.PI - properties.orientation) % (2 * Math.PI);
+          let style;
+          if (rotation >= Math.PI / 2 && rotation <= (3 * Math.PI) / 2) {
+            style = busTypes[option].right.clone();
+            rotation += Math.PI;
+          } else {
+            style = busTypes[option].left.clone();
+          }
+
+          const image = style.getImage();
+          image.setRotation(rotation);
 
           return style;
         }
