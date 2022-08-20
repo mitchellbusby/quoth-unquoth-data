606a2b856e12b3bb9153e270a2e73e47aa95f172
Mitchell Busby <mitchbusby@gmail.com>
2022-08-20 14:32:35 +1000

    add time travel with rotary now

--- app/src/Clock.tsx
+++ app/src/Clock.tsx
@@ -1,7 +1,9 @@
+import CircularSlider from "@fseehawer/react-circular-slider";
 import { useContext, useEffect, useMemo, useState } from "react";
 import { AppStateContext } from "./AppState";
-import { DateTime, Duration } from "luxon";
+import { DateTime } from "luxon";
 import { Button } from "./components/Button";
+import { css } from "@emotion/react";
 
 const Clock = () => {
   const appState = useContext(AppStateContext);
@@ -18,7 +20,7 @@ const Clock = () => {
   const timeString = useMemo(() => {
     const date = DateTime.fromSeconds(timeOfDay, { zone: "UTC" });
 
-    return date.toFormat("HH:mm a");
+    return date.toFormat("hh:mm a");
   }, [timeOfDay]);
 
   const makeHandleTimeTravelClick = (delta: number) => () => {
@@ -39,16 +41,37 @@ const Clock = () => {
         gap: "4px",
       }}
     >
-      <div>Time of day</div>
-      <div>{timeString}</div>
-      <div
-        css={{
-          display: "flex",
-          gap: "4px",
-        }}
-      >
-        <Button onClick={makeHandleTimeTravelClick(-10 * 60)}>-10m</Button>
-        <Button onClick={makeHandleTimeTravelClick(10 * 60)}>+10m</Button>
+      <div>
+        <CircularSlider
+          min={0}
+          max={86400}
+          onChange={(value: number) => {
+            setTimeOfDay(value);
+          }}
+          dataIndex={timeOfDay}
+          width={150}
+          renderLabelValue={
+            <div
+              css={css`
+                position: absolute;
+                top: 0px;
+                left: 0px;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                color: rgb(39, 43, 119);
+                user-select: none;
+                z-index: 1;
+                font-size: 24px;
+              `}
+            >
+              {timeString}
+            </div>
+          }
+        />
       </div>
     </div>
   );
